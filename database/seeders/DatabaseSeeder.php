46108a2b8c5504c2f10005375c65f7e8de3b6d14
sara <sarahaladwar@gmail.com>
2023-12-12 19:48:24 +0100

    session 10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Car;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,8 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        //creating users table through factories
+        User::factory(10)->create();
+        Car::factory(10)->create();
 
+
+        //creating users table directly/manually
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
